5ceff684d237910f2b652902bdfe37788dce57d0
Pavel Plotnikov <anndpowec@mail.ru>
2021-09-16 20:15:23 +0300

    Inittask7_main c

---
+++ task7_main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include "func.h"
+
+void main ()
+{
+	x = 1;
+
+	function(x);
+
+	printf("x = %.4lf\n", x);
+
+	printf("f = %.4lf\n\n", result);
+
+	printf("x =");
+
+	scanf("%lf", &x);
+
+	function(x);
+
+	printf("f = %.4lf", result);
+
+}
