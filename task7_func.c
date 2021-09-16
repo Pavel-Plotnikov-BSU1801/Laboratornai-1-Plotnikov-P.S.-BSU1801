fbe0b392931da9fda1936213794da914c454b0a1
Pavel Plotnikov <anndpowec@mail.ru>
2021-09-16 20:14:42 +0300

    Inittask7 c

---
+++ task7_func.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include "func.h"
+
+double x,result;
+void function()
+
+{
+	result = pow(((1 + x + pow(x, 2)) / (2 * x + pow(x, 2)) + 2 - (1 - x - pow(x, 2)) / (2 * x - pow(x, 2))), -1) * (5 - 2 * pow(x, 2));
+
+}
