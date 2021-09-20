ce49bed17f5653a34801fa986a40806abccc2b05
Pavel-Plotnikov-BSU1801 <90856232+Pavel-Plotnikov-BSU1801@users.noreply.github.com>
2021-09-20 14:18:09 +0300

    Add files via upload

---
+++ task7/task7_func.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include "func.h"
+
+double x,result;
+
+void f(void)
+
+{
+	result = pow(((1 + x + pow(x, 2)) / (2 * x + pow(x, 2)) + 2 - (1 - x - pow(x, 2)) / (2 * x - pow(x, 2))), -1) * (5 - 2 * pow(x, 2));
+
+}
